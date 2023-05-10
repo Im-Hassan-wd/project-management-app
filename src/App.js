d0b986d8a45192789980ecd4b3dc36203943e66c
huzitaiwo <alhassansahih@gmail.com>
2023-05-10 17:45:54 +0100

    sidebar and navbar components

--- src/App.js
+++ src/App.js
@@ -7,28 +7,34 @@ import Create from "./pages/create/Create";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Project from "./pages/project/Project";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/project">
-            <Project />
-          </Route>
-        </Switch>
+        <Sidebar />
+        <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+          </Switch>
+        </div>
       </BrowserRouter>
     </div>
   );
